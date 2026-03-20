03debaae991d529657d80e81819a4f691f15d531
Jeciel Treska <jecieltreska@gmail.com>
2026-03-20 10:10:56 -0300

    melhorado nav mobile

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,5 +1,5 @@
-const qsa = s => Array.from(document.querySelectorAll(s)) 
-
+const qsa = s => Array.from(document.querySelectorAll(s)) ;
+const qs = s => document.querySelector(s);
 const reveals = document.querySelectorAll('.reveal');
 
 window.addEventListener('scroll', () => {
@@ -47,3 +47,54 @@ function restartAnimation() {
   // Substitui o antigo pelo novo
   container.parentNode.replaceChild(clone, container);
 }
+
+/* Menu celuluar - abrir e fechar */
+const iconeCelular = qs('#navCelular');
+const itensMenu = qs('#menuCelular');
+
+iconeCelular?.addEventListener('click', () => {
+  const espandir = iconeCelular.getAttribute('aria-expanded') === 'true';//valida se esta aberto o menu
+  iconeCelular.setAttribute('aria-expanded', String(!espandir)); // altera atributo
+  //alternar visibilidade do menu
+  if(itensMenu.classList.contains('hidden')) itensMenu.classList.remove('hidden');
+  else itensMenu.classList.add('hidden');
+})
+
+//destaca iten ativo do menu
+const navLinks = qsa('.nav-link');
+const section = navLinks
+  .map(link => {
+    const id = link.getAttribute('href')?.replace('#', "");
+    return document.getElementById(id)
+  })
+  .filter(Boolean);
+
+const HEADER_OFFSET = 120;
+
+function onScrollSpy() {
+  let currentSection = null;
+  let minDistance= Infinity;
+
+  section.forEach(section => {
+    const rect = section.getBoundingClientRect();
+    const distance = Math.abs(rect.top - HEADER_OFFSET);
+
+    if(rect.top <= HEADER_OFFSET + 50 && distance< minDistance){
+      minDistance = distance;
+      currentSection = section;
+    }
+  });
+  
+  navLinks.forEach(link => link.classList.remove('active'));
+
+  if( currentSection ){
+    const activeLink = document.querySelector(
+     `.nav-link[href="#${currentSection.id}"]`
+    );
+    activeLink?.classList.add('active');
+  }
+}
+
+window.addEventListener('scroll', onScrollSpy, {passive: true});
+window.addEventListener('resize', onScrollSpy);
+onScrollSpy();
